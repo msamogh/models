170ce8d1e858979ca13a95b4d247f9d6b022a304
root <msamogh@gmail.com>
2018-04-02 10:06:19 +0000

    Converted load_estimator.py to function

--- load_estimator.py
+++ load_estimator.py
@@ -15,25 +15,25 @@ from memory_profiler import profile
 
 import random
 
-predict_fn = predictor.from_saved_model(
-    'resnet_clf_tf_estimator/1522231372/',
-    signature_def_key='probabilities'
-)
+def print_prediction(model_timestamp):
+    predict_fn = predictor.from_saved_model(
+        'resnet_clf_tf_estimator/' + model_timestamp + '/',
+        signature_def_key='probabilities'
+    )
 
-from official.resnet.cifar10_main import input_fn
-next_element = input_fn(
-    False,
-    data_dir='/tmp/cifar10_data',
-    batch_size=128
-)[0]
-with tf.Session() as sess:
-    # iterator = dataset.make_one_shot_iterator()
-    # next_element = iterator.get_next()[0]
-    # next_element = sess.run(next_element)
+    from official.resnet.cifar10_main import input_fn
+    next_element = input_fn(
+        False,
+        data_dir='/tmp/cifar10_data',
+        batch_size=128
+    )[0]
+    with tf.Session() as sess:
+        # iterator = dataset.make_one_shot_iterator()
+        # next_element = iterator.get_next()[0]
+        # next_element = sess.run(next_element)
 
-    predictions = predict_fn({
-        'input': next_element
-    })
-    print(predictions['output'][0])
-    print(len(predictions['output'][0]))
-
+        predictions = predict_fn({
+            'input': next_element
+        })
+        print(predictions['output'][0])
+        print(len(predictions['output'][0]))
